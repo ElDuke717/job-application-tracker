014bfbd70a616fdf932d5ee1b402b76eec6f7a0c
elduke717 <spacematcha@proton.me>
2023-12-15 13:55:26 -0500

    set up files

---
+++ src/types/jobApplication.ts
@@ -0,0 +1,19 @@
+export interface JobApplication {
+    dateSubmitted: Date;
+    company: string;
+    jobTitle: string;
+    location: string;
+    applicationStatus: string;
+    applicationType: string;
+    resume: string; // URL or file path
+    coverLetter: string; // URL or file path
+    jobPostingURL: string;
+    internalContact: {
+      name: string;
+      title: string;
+    };
+    internalContactEmail: string;
+    doubleDown: boolean;
+    notesComments: string;
+  }
+  
